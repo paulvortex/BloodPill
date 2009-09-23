39f77c0f9a4c4364b25b9ea8c632b224b519ef41
paulvortex <paul.vortex@gmail.com>
2009-09-23 08:30:28 +0000

    Blood Pill - added Andrey [Rackot] Grachev to credits

--- src/bloodpill.h
+++ src/bloodpill.h
@@ -1,6 +1,6 @@
 #include "cmdlib.h"
 
-#define BLOODPILL_WELCOME	"----------------------------------------\nBlood Pill v0.3rc1 by VorteX and Mean Person\nA set of utils to manipulate Blood Omen: Legacy Of Kain files\nportions of code by LordHavoc\nAdditional thanks to Balder and Zench from XentaX forums (www.xentax.com)\nTIM specs by Klarth and Raul Sobon\n----------------------------------------\n"
+#define BLOODPILL_WELCOME	"----------------------------------------\nBlood Pill v0.3rc1 by VorteX and Mean Person\nA set of utils to manipulate Blood Omen: Legacy Of Kain files\nportions of code by LordHavoc\nAdditional thanks to Balder and Zench from XentaX forums (www.xentax.com)\nTIM specs by Klarth and Raul Sobon\nBigfile specs by Andrey [Rackot] Grachev\n----------------------------------------\n"
 #define MAX_BLOODPATH 512
 
 // global switches
