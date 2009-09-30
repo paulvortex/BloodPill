6f59ebf95f4705f06bd92642ef9470e28f56753c
paulvortex <paul.vortex@gmail.com>
2009-09-30 15:21:50 +0000

    cleaning shit about cryptfloor textures

--- rawall/a_raw.bat
+++ rawall/a_raw.bat
@@ -2,5 +2,5 @@
 del bpill.exe /q
 copy .\..\bpill.exe bpill.exe /y
 
-for %%i in (*.dat) do bpill -nc -raw %%i %%i.tga -type 0 -width 32 -height 400 -offset 0 -bytes 1
+for %%i in (*.dat) do bpill -nc -raw %%i %%i.tga -type 0 -width 32 -height 2500 -offset 0 -bytes 1
 pause
