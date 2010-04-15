fe2052e77139a951dfbd757988984324b0c00aec
paulvortex <paul.vortex@gmail.com>
2010-04-15 14:52:41 +0000

    making subtitles eventually let me create proper Control Mind spell, working on it. At moment flay artifacts are replaced with control mind effect. Remove some unused files.

--- RAW/77691833_typeZ.bat
+++ RAW/77691833_typeZ.bat
@@ -2,12 +2,12 @@
 del bpill.exe /q
 copy .\..\bpill.exe bpill.exe /y
 
-SET W=24
+SET W=1
 
 :LOOP
-IF "%W%"=="25" ( GOTO EXIT )
-bpill -nc -raw 75681834_typeZ.dat 75681834_typeZ.tga -type 7
-bpill -nc -raw 75681834_typeZ.dat 75681834_typeZ_2.tga -type 0 -width 72 -height 1400 -offset 0 -bytes 1
+IF "%W%"=="100" ( GOTO EXIT )
+REM bpill -nc -raw 75681834_typeZ.dat 75681834_typeZ.tga -type 7
+bpill -nc -raw 75681834_typeZ.dat 75681834_typeZ_2_%W%.tga -type 0 -width %W% -height 500 -offset 0 -bytes 1
 
 set /a W=W+1
 GOTO LOOP
