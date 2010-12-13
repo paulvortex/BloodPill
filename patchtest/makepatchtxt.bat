d1a46342f18e68e11b5da62c1147cea4a3c01be8
paulvortex <paul.vortex@gmail.com>
2010-12-13 22:21:40 +0000

    compiler Qlinar's new hires inventory, with proper widescreen support.

--- patchtest/makepatchtxt.bat
+++ patchtest/makepatchtxt.bat
@@ -1,6 +1,6 @@
 REM script to generate patch list for folder
 @echo off
 IF EXIST patch.txt del /Q patch.txt
-for %%i IN (files\*) DO (
- echo %%i >> patch.txt
+for %%i IN (files2\*) DO (
+ echo RAW #%%~ni %%i >> patch.txt
 )
